c9bdd390d1ae0da478d0d00d3bc7513991315b44
favorable-vector-1743 <epzads@gmail.com>
2019-02-17 18:26:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board,index)
 
 
-if board[index] == " "
+if board[index] == ""
 
   return true
 
@@ -10,22 +10,23 @@ else
 
   return false
 
+end
 end 
 
 
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board,index)
-
-if board[index] == "X" || board[index] == "O"
-
-  return true
-
-else
-
-  return false
-
-end
-
-end
+# def position_taken?(board,index)
+#
+# if board[index] == "X" || board[index] == "O"
+#
+#   return true
+#
+# else
+#
+#   return false
+#
+# end
+#
+# end
